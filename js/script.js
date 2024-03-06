100a475c73b76a70ea692b0e97dc60c8d7563377
DionisiePulbere <dionisiepulbere@gmail.com>
2024-03-06 17:47:58 +0100

    feat: add html css js

--- js/script.js
+++ js/script.js
@@ -19,4 +19,90 @@
 // essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). 
 // Questo richiederà un minimo di ricerca.
 
-// Raccogliere i dati
+// Raccogliere i dati quando il cliente clicca sul bottone 
+const clcButton = document.querySelector('#clc-button');
+clcButton.addEventListener('click', function() {
+
+    const userName = document.querySelector('#user-name').value;
+    const userKm = parseInt(document.querySelector('#user-km').value);
+    const userAge = parseInt(document.querySelector('#user-age').value);
+    console.log(userName);
+    console.log(userKm);
+    console.log(userAge);
+
+    //Definiamo il costo al Km    
+    const priceKm = 0.21 ;
+    
+    //Calcoliamo il costo dei Km 
+    const totKmPrice = userKm * priceKm ;
+    console.log(totKmPrice);
+
+    // Defianimo gli sconti  
+    const discountTeen = 20;
+    const discountOld = 40;
+
+    // Definiamo le condizioni dello sconto 
+    let price = totKmPrice;
+
+    let discount;
+
+    if(userAge < 18 ){
+        price = totKmPrice - (totKmPrice * discountTeen / 100);
+        discount = `Sconto del ${discountTeen}%`;
+    } else if (userAge > 65 ){
+        price = totKmPrice - (totKmPrice * discountOld / 100);
+        discount = `Sconto del ${discountOld}%`;
+    } else {
+        discount = `Biglietto Standard`;
+    }
+
+    // Arrotondiamo il risultato a 2 decimali 
+    const ticketPrice =price.toFixed(2); 
+
+    // Generiamo il numero della carrozza e il codice CP
+    let carriage ;
+
+    carriage = Math.floor(Math.random() * 16) + 1;
+
+    let codeCp ;
+
+    codeCp = Math.floor(Math.random() * 99999) + 10000;
+
+    // Stampiano il risultato 
+    const userMessageName = document.querySelector('#user-message-name');
+    userMessageName.innerHTML = userName;
+
+    const userMessageDiscount = document.querySelector('#user-discount');
+    userMessageDiscount.innerHTML = discount;
+
+    const userMessageCarriage = document.querySelector('#user-carriage');
+    userMessageCarriage.innerHTML = carriage;
+
+    const userMessageCodeCp = document.querySelector('#user-code');
+    userMessageCodeCp.innerHTML = codeCp;
+
+    const userMessageTicketPrice = document.querySelector('#user-ticket-price');
+    userMessageTicketPrice.innerHTML = ticketPrice + "€";
+});
+
+const clearButton = document.querySelector('#clear-button');
+clearButton.addEventListener('click', function() {
+    document.querySelector("#user-name").value = "";
+    document.querySelector("#user-km").value = "";
+    document.querySelector("#user-age").value = "";
+
+    const userMessageName = document.querySelector('#user-message-name');
+    userMessageName.innerHTML = "";
+
+    const userMessageDiscount = document.querySelector('#user-discount');
+    userMessageDiscount.innerHTML = "";
+
+    const userMessageCarriage = document.querySelector('#user-carriage');
+    userMessageCarriage.innerHTML = "";
+
+    const userMessageCodeCp = document.querySelector('#user-code');
+    userMessageCodeCp.innerHTML = "";
+
+    const userMessageTicketPrice = document.querySelector('#user-ticket-price');
+    userMessageTicketPrice.innerHTML = "";
+});
